1ddd39683ae447b2c6778e229a571a2899624fa0
NicoPinoVergara <nicolas.pinovergara@gmail.com>
2024-08-24 22:59:42 -0400

    Agregar archivos de proyecto.

---
+++ CalculadoraEVA1/Form1.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        BT1 = New Button()
+        BT2 = New Button()
+        BT3 = New Button()
+        BT4 = New Button()
+        BT5 = New Button()
+        BT6 = New Button()
+        BT7 = New Button()
+        BT8 = New Button()
+        BT9 = New Button()
+        BT0 = New Button()
+        BTbarrer = New Button()
+        BTigual = New Button()
+        BTrestar = New Button()
+        BTMultiplicar = New Button()
+        BTdividir = New Button()
+        BTsuma = New Button()
+        txtdigito = New TextBox()
+        SuspendLayout()
+        ' 
+        ' BT1
+        ' 
+        BT1.Location = New Point(88, 299)
+        BT1.Name = "BT1"
+        BT1.Size = New Size(94, 29)
+        BT1.TabIndex = 0
+        BT1.Text = "1"
+        BT1.UseVisualStyleBackColor = True
+        ' 
+        ' BT2
+        ' 
+        BT2.Location = New Point(212, 299)
+        BT2.Name = "BT2"
+        BT2.Size = New Size(94, 29)
+        BT2.TabIndex = 1
+        BT2.Text = "2"
+        BT2.UseVisualStyleBackColor = True
+        ' 
+        ' BT3
+        ' 
+        BT3.Location = New Point(333, 299)
+        BT3.Name = "BT3"
+        BT3.Size = New Size(94, 29)
+        BT3.TabIndex = 2
+        BT3.Text = "3"
+        BT3.UseVisualStyleBackColor = True
+        ' 
+        ' BT4
+        ' 
+        BT4.Location = New Point(88, 253)
+        BT4.Name = "BT4"
+        BT4.Size = New Size(94, 29)
+        BT4.TabIndex = 3
+        BT4.Text = "4"
+        BT4.UseVisualStyleBackColor = True
+        ' 
+        ' BT5
+        ' 
+        BT5.Location = New Point(212, 253)
+        BT5.Name = "BT5"
+        BT5.Size = New Size(94, 29)
+        BT5.TabIndex = 4
+        BT5.Text = "5"
+        BT5.UseVisualStyleBackColor = True
+        ' 
+        ' BT6
+        ' 
+        BT6.Location = New Point(333, 253)
+        BT6.Name = "BT6"
+        BT6.Size = New Size(94, 29)
+        BT6.TabIndex = 5
+        BT6.Text = "6"
+        BT6.UseVisualStyleBackColor = True
+        ' 
+        ' BT7
+        ' 
+        BT7.Location = New Point(88, 202)
+        BT7.Name = "BT7"
+        BT7.Size = New Size(94, 29)
+        BT7.TabIndex = 6
+        BT7.Text = "7"
+        BT7.UseVisualStyleBackColor = True
+        ' 
+        ' BT8
+        ' 
+        BT8.Location = New Point(212, 202)
+        BT8.Name = "BT8"
+        BT8.Size = New Size(94, 29)
+        BT8.TabIndex = 7
+        BT8.Text = "8"
+        BT8.UseVisualStyleBackColor = True
+        ' 
+        ' BT9
+        ' 
+        BT9.Location = New Point(333, 202)
+        BT9.Name = "BT9"
+        BT9.Size = New Size(94, 29)
+        BT9.TabIndex = 8
+        BT9.Text = "9"
+        BT9.UseVisualStyleBackColor = True
+        ' 
+        ' BT0
+        ' 
+        BT0.Location = New Point(88, 345)
+        BT0.Name = "BT0"
+        BT0.Size = New Size(94, 53)
+        BT0.TabIndex = 9
+        BT0.Text = "0"
+        BT0.UseVisualStyleBackColor = True
+        ' 
+        ' BTbarrer
+        ' 
+        BTbarrer.Location = New Point(212, 357)
+        BTbarrer.Name = "BTbarrer"
+        BTbarrer.Size = New Size(94, 29)
+        BTbarrer.TabIndex = 10
+        BTbarrer.Text = "C"
+        BTbarrer.UseVisualStyleBackColor = True
+        ' 
+        ' BTigual
+        ' 
+        BTigual.Location = New Point(333, 357)
+        BTigual.Name = "BTigual"
+        BTigual.Size = New Size(94, 29)
+        BTigual.TabIndex = 11
+        BTigual.Text = "="
+        BTigual.UseVisualStyleBackColor = True
+        ' 
+        ' BTrestar
+        ' 
+        BTrestar.Location = New Point(470, 299)
+        BTrestar.Name = "BTrestar"
+        BTrestar.Size = New Size(94, 29)
+        BTrestar.TabIndex = 12
+        BTrestar.Text = "-"
+        BTrestar.UseVisualStyleBackColor = True
+        ' 
+        ' BTMultiplicar
+        ' 
+        BTMultiplicar.Location = New Point(470, 253)
+        BTMultiplicar.Name = "BTMultiplicar"
+        BTMultiplicar.Size = New Size(94, 29)
+        BTMultiplicar.TabIndex = 13
+        BTMultiplicar.Text = "x"
+        BTMultiplicar.UseVisualStyleBackColor = True
+        ' 
+        ' BTdividir
+        ' 
+        BTdividir.Location = New Point(470, 202)
+        BTdividir.Name = "BTdividir"
+        BTdividir.Size = New Size(94, 29)
+        BTdividir.TabIndex = 14
+        BTdividir.Text = "/"
+        BTdividir.UseVisualStyleBackColor = True
+        ' 
+        ' BTsuma
+        ' 
+        BTsuma.Location = New Point(470, 357)
+        BTsuma.Name = "BTsuma"
+        BTsuma.Size = New Size(94, 29)
+        BTsuma.TabIndex = 15
+        BTsuma.Text = "+"
+        BTsuma.UseVisualStyleBackColor = True
+        ' 
+        ' txtdigito
+        ' 
+        txtdigito.Location = New Point(90, 157)
+        txtdigito.Name = "txtdigito"
+        txtdigito.Size = New Size(337, 27)
+        txtdigito.TabIndex = 16
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtdigito)
+        Controls.Add(BTsuma)
+        Controls.Add(BTdividir)
+        Controls.Add(BTMultiplicar)
+        Controls.Add(BTrestar)
+        Controls.Add(BTigual)
+        Controls.Add(BTbarrer)
+        Controls.Add(BT0)
+        Controls.Add(BT9)
+        Controls.Add(BT8)
+        Controls.Add(BT7)
+        Controls.Add(BT6)
+        Controls.Add(BT5)
+        Controls.Add(BT4)
+        Controls.Add(BT3)
+        Controls.Add(BT2)
+        Controls.Add(BT1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents BT1 As Button
+    Friend WithEvents BT2 As Button
+    Friend WithEvents BT3 As Button
+    Friend WithEvents BT4 As Button
+    Friend WithEvents BT5 As Button
+    Friend WithEvents BT6 As Button
+    Friend WithEvents BT7 As Button
+    Friend WithEvents BT8 As Button
+    Friend WithEvents BT9 As Button
+    Friend WithEvents BT0 As Button
+    Friend WithEvents BTbarrer As Button
+    Friend WithEvents BTigual As Button
+    Friend WithEvents BTrestar As Button
+    Friend WithEvents BTMultiplicar As Button
+    Friend WithEvents BTdividir As Button
+    Friend WithEvents BTsuma As Button
+    Friend WithEvents txtdigito As TextBox
+
+End Class
---
+++ CalculadoraEVA1/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+    Dim primerNum As Double
+    Dim segundoNum As Double
+    Dim operacion As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BT0_Click(sender As Object, e As EventArgs) Handles BT0.Click
+        txtdigito.Text = txtdigito.Text & "0"
+    End Sub
+
+    Private Sub BTbarrer_Click(sender As Object, e As EventArgs) Handles BTbarrer.Click
+        txtdigito.Text = ""
+    End Sub
+
+    Private Sub BTdividir_Click(sender As Object, e As EventArgs) Handles BTdividir.Click
+        primerNum = txtdigito.Text
+        txtdigito.Text = ""
+        operacion = "/"
+    End Sub
+
+    Private Sub BTMultiplicar_Click(sender As Object, e As EventArgs) Handles BTMultiplicar.Click
+        primerNum = txtdigito.Text
+        txtdigito.Text = ""
+        operacion = "x"
+    End Sub
+
+    Private Sub BT1_Click(sender As Object, e As EventArgs) Handles BT1.Click
+        txtdigito.Text = txtdigito.Text & "1"
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtdigito.TextChanged
+
+    End Sub
+
+    Private Sub BT2_Click(sender As Object, e As EventArgs) Handles BT2.Click
+        txtdigito.Text = txtdigito.Text & "2"
+    End Sub
+
+    Private Sub BT3_Click(sender As Object, e As EventArgs) Handles BT3.Click
+        txtdigito.Text = txtdigito.Text & "3"
+    End Sub
+
+    Private Sub BT4_Click(sender As Object, e As EventArgs) Handles BT4.Click
+        txtdigito.Text = txtdigito.Text & "4"
+    End Sub
+
+    Private Sub BT5_Click(sender As Object, e As EventArgs) Handles BT5.Click
+        txtdigito.Text = txtdigito.Text & "5"
+    End Sub
+
+    Private Sub BT6_Click(sender As Object, e As EventArgs) Handles BT6.Click
+        txtdigito.Text = txtdigito.Text & "6"
+    End Sub
+
+    Private Sub BT7_Click(sender As Object, e As EventArgs) Handles BT7.Click
+        txtdigito.Text = txtdigito.Text & "7"
+    End Sub
+
+    Private Sub BT8_Click(sender As Object, e As EventArgs) Handles BT8.Click
+        txtdigito.Text = txtdigito.Text & "8"
+    End Sub
+
+    Private Sub BT9_Click(sender As Object, e As EventArgs) Handles BT9.Click
+        txtdigito.Text = txtdigito.Text & "9"
+    End Sub
+
+    Private Sub BTsuma_Click(sender As Object, e As EventArgs) Handles BTsuma.Click
+        primerNum = txtdigito.Text
+        txtdigito.Text = ""
+        operacion = "+"
+    End Sub
+
+    Private Sub BTrestar_Click(sender As Object, e As EventArgs) Handles BTrestar.Click
+        primerNum = txtdigito.Text
+        txtdigito.Text = ""
+        operacion = "-"
+    End Sub
+
+    Private Sub BTigual_Click(sender As Object, e As EventArgs) Handles BTigual.Click
+        segundoNum = txtdigito.Text
+        Select Case operacion
+            Case "+"
+                txtdigito.Text = primerNum + segundoNum
+            Case "-"
+                txtdigito.Text = primerNum - segundoNum
+            Case "x"
+                txtdigito.Text = primerNum * segundoNum
+            Case "/"
+                If segundoNum = 0 Then
+                    txtdigito.Text = "No se puede divir entre 0"
+                Else
+                    txtdigito.Text = primerNum / segundoNum
+                End If
+        End Select
+    End Sub
+End Class
